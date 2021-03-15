26062b3a17d3bf75a222c56c11a573d27aa87324
Ava Thorn <avatheavian@gmail.com>
2021-03-14 21:27:39 -0400

    update auth

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -57,9 +57,14 @@ var WildRydes = window.WildRydes || {};
             Name: 'email',
             Value: email
         };
+        var dataUser = {
+            Name: 'preferred_username',
+            Value: email.split('@')[0]
+        };
         var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+        var attributeUser = new AmazonCognitoIdentity.CognitoUserAttribute(dataUser);
 
-        userPool.signUp(toUsername(email), password, [attributeEmail], null,
+        userPool.signUp(toUsername(email), password, [attributeEmail, attributeUser], null,
             function signUpCallback(err, result) {
                 if (!err) {
                     onSuccess(result);
